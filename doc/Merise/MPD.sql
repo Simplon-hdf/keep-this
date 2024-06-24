5517acd75ab62d217929542f1a63f8061ac3f80a
Talique <ropars.k@gmail.com>
2024-06-24 11:44:21 +0200

    doc:update merise add sharing et historify tables

--- doc/Merise/MPD.sql
+++ doc/Merise/MPD.sql
@@ -1,7 +1,7 @@
-CREATE TABLE users(
+CREATE TABLE Users(
     id_user VARCHAR(36) ,
     nom_user VARCHAR(50)  NOT NULL,
-    certified_email_user VARCHAR(100)  NOT NULL,
+    certified_email_user VARCHAR(100) ,
     temp_email_user VARCHAR(100)  NOT NULL,
     password_user VARCHAR(100)  NOT NULL,
     salt_user VARCHAR(100)  NOT NULL,
@@ -12,17 +12,14 @@ CREATE TABLE users(
 
 CREATE TABLE notes(
     id_note SERIAL,
-    content_content TEXT NOT NULL,
     create_at TIMESTAMP NOT NULL,
+    content_content TEXT NOT NULL,
     update_at TIMESTAMP NOT NULL,
     archived_at TIMESTAMP NOT NULL,
     status_note VARCHAR(50) ,
-    previous_note INTEGER NOT NULL,
     id_user VARCHAR(36) ,
     PRIMARY KEY(id_note),
-    UNIQUE(previous_note),
-    FOREIGN KEY(previous_note) REFERENCES notes(id_note),
-    FOREIGN KEY(id_user) REFERENCES users(id_user)
+    FOREIGN KEY(id_user) REFERENCES Users(id_user)
 );
 
 CREATE TABLE notebooks(
@@ -31,7 +28,7 @@ CREATE TABLE notebooks(
     created_at TIMESTAMP NOT NULL,
     id_user VARCHAR(36) ,
     PRIMARY KEY(id_notebook),
-    FOREIGN KEY(id_user) REFERENCES users(id_user)
+    FOREIGN KEY(id_user) REFERENCES Users(id_user)
 );
 
 CREATE TABLE admin(
@@ -48,7 +45,7 @@ CREATE TABLE authentification_tokens(
     timestamp_token TIMESTAMP NOT NULL,
     id_user VARCHAR(36) ,
     PRIMARY KEY(token),
-    FOREIGN KEY(id_user) REFERENCES users(id_user)
+    FOREIGN KEY(id_user) REFERENCES Users(id_user)
 );
 
 CREATE TABLE friendships(
@@ -56,17 +53,33 @@ CREATE TABLE friendships(
     id_acceptor VARCHAR(36) ,
     id_requestor VARCHAR(36) ,
     PRIMARY KEY(Id_friendship),
-    FOREIGN KEY(id_acceptor) REFERENCES users(id_user),
-    FOREIGN KEY(id_requestor) REFERENCES users(id_user)
+    FOREIGN KEY(id_acceptor) REFERENCES Users(id_user),
+    FOREIGN KEY(id_requestor) REFERENCES Users(id_user)
 );
 
-CREATE TABLE share(
-    id_user VARCHAR(36) ,
-    id_note INTEGER,
-    write_rule BOOLEAN,
-    PRIMARY KEY(id_user, id_note),
-    FOREIGN KEY(id_user) REFERENCES users(id_user),
+CREATE TABLE share_status(
+    title_status VARCHAR(50) ,
+    PRIMARY KEY(title_status)
+);
+
+CREATE TABLE note_versions(
+    id_note_versions SERIAL,
+    created_at TIMESTAMP NOT NULL,
+    note_content TEXT NOT NULL,
+    id_note INTEGER NOT NULL,
+    PRIMARY KEY(id_note_versions),
     FOREIGN KEY(id_note) REFERENCES notes(id_note)
+);
+
+CREATE TABLE sharing(
+    id_pooling SERIAL,
+    id_note INTEGER,
+    title_status VARCHAR(50) ,
+    id_user VARCHAR(36) ,
+    PRIMARY KEY(id_pooling),
+    FOREIGN KEY(id_note) REFERENCES notes(id_note),
+    FOREIGN KEY(title_status) REFERENCES share_status(title_status),
+    FOREIGN KEY(id_user) REFERENCES Users(id_user)
 );
 
 CREATE TABLE contain(
